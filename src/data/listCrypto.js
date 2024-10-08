62235e4bf6def3657df66feecbc2edd79d98f441
Arón Díaz <arondiaz.cpn@gmail.com>
2024-10-08 16:20:28 -0300

    added list of crypto for map options

---
+++ src/data/listCrypto.js
@@ -0,0 +1,17 @@
+const listCrypto = {
+    bitcoin: "bitcoin",
+    ethereum: "ethereum",
+    ripple: "ripple",
+    cardano: "cardano",
+    dogecoin: "dogecoin",
+    solana: "solana",
+    tron: "tron",
+    polkadot: "polkadot",
+    litecoin: "litecoin",
+    dai: "dai",
+    chainlink: "chainlink",
+    cosmos: "cosmos",
+  };
+  
+
+  export default listCrypto;
